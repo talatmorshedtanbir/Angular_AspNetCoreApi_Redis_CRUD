643ee4ef20c4f7d5e30fa68ec5cc947d097eed0e
hossaintanbir0 <hossaintanbir0@gmail.com>
2021-06-20 12:17:23 +0600

    Added new client project

--- RedisCRUD+Angular/RedisClient/src/app/app.module.ts
+++ RedisCRUD+Angular/RedisClient/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    EmployeeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
